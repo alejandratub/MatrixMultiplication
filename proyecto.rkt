ba008f81080cbc20679dce9a453d6694a1770a87
alejandratub <alejandra.tubilla@gmail.com>
2019-11-19 14:11:27 -0600

    Multiplication

--- proyecto.rkt
+++ proyecto.rkt
@@ -8,13 +8,15 @@
     Alejandra Tubilla Castellanos
     A01022960
 |#
+
+;Libraries to use matrix
 (require htdp/matrix)
+(require math/matrix)
   
 (define (read_file )
   (let
     ;Read from file and convert to list
     ([listF (file->list "matriz.txt")])
-    (displayln listF)
     ;Get the number of rows in the first matrix
     (define row1 (list-ref listF 0))
     ;Get the number of columns in the first matrix
@@ -25,53 +27,43 @@
     (define row2 (list-ref listF indexL))
     ;Get the number of columns in the second matrix
     (define col2 (list-ref listF (+ 1 indexL)))
-
-    (display "Row 1: ")
-      (displayln row1)
-      (display "Col 1: ")
-      (displayln col1)
-      (display "Row 2: ")
-      (displayln row2)
-      (display "Col 2: ")
-      (displayln col2)
     ;Split the list with all the information into the separate matrix
     (let-values (((list1 list2) (split-at listF indexL)))
 
         ;Get only the elements of the matrix
         (define firstM  (cdr (cdr list1)))
         (define secondM (cdr (cdr list2)))
-      
+        
+        ;Form first matrix
+        (define Matrix1 
+        (let-values (((part1 part2) (split-at firstM row1)))
+          (list part1 part2)
+        ))
 
+        ;Form second matrix
+        (define Matrix2
+        (let-values (((part1 part2) (split-at secondM row2)))
+          (list part1 part2)
+        ))
 
-          
-        (display "First matrix: ")
-        (displayln firstM)
-        (display "First matrix: ")
-        (displayln secondM)
+        (display "Matrix 1: ")
+        (displayln Matrix1)
 
-      ;Create matrix from list 
-      (define Matrix1 (make-matrix row1 col1 firstM))
-      (define Matrix2 (make-matrix row2 col2 secondM))
-      (matrix-render Matrix1)
-      ;(matrix-render Matrix2)
-    
-      
+        (display "Matrix 2: ")
+        (displayln Matrix2)
+
+        (display "Result: ")
+        (m-mult Matrix1 Matrix2)
       )
-       
-  
-    
-      
-
-    
   )
 )
 
 
 ;(call-with-input-file "matriz.txt" read_file)
 
-(define (m-mult m1 m2)
-  (for/list ([r m1])
-    (for/list ([c (apply map list m2)])
+(define (m-mult Matrix1 Matrix2)
+  (for/list ([r Matrix1])
+    (for/list ([c (apply map list Matrix2)])
       (apply + (map * r c)))))
 
 ; (m-mult '((1 2) (3 4)) '((5 6) (7 8)))
