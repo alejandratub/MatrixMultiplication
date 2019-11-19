afba1cdc19790959012585b87660e9d40fd2ee18
alejandratub <alejandra.tubilla@gmail.com>
2019-11-19 12:26:48 -0600

    Read from file and create lists

--- proyecto.rkt
+++ proyecto.rkt
@@ -1,27 +1,61 @@
 #|
-Matrix Multiplication in Racket
+    Matrix Multiplication in Racket
     valid estensions: .rkt or .scm
 
     Aarón Zajac Hadid
     A01023376
 
-    Alejandra Tubilla
+    Alejandra Tubilla Castellanos
     A01022960
 |#
 
+  
+(define (read_file )
+  (let
+    ;Read from file and convert to list
+    ([listF (file->list "matriz.txt")])
+    (displayln listF)
+    ;Get the number of rows in the first matrix
+    (define row1 (list-ref listF 0))
+    ;Get the number of columns in the first matrix
+    (define col1 (list-ref listF 1))
+    ;Get index to know where to divide the lists
+    (define indexL (+ 2 (* row1 col1)))
+    ;Get the number of rows in the second matrix
+    (define row2 (list-ref listF indexL))
+    ;Get the number of columns in the second matrix
+    (define col2 (list-ref listF (+ 1 indexL)))
 
-; #lang racket
-;Read File line by line
-(define (read_file file)
-  (let ((line (read-line file 'any)))
-    (unless (eof-object? line)
-    ; (display (string? line))
-    (if (string=? line "-")
-      (displayln line)
-      (display line))
-    (read_file file))))
+    ;Split the list with all the information into the separate matrix
+    (let-values (((list1 list2) (split-at listF indexL)))
 
-(call-with-input-file "matriz.txt" read_file)
+    ;Get only the elements of the matrix
+    (define firstM  (cdr (cdr list1)))
+    (define secondM (cdr (cdr list2)))
+
+
+
+      
+    (display "First matrix: ")
+    (displayln firstM)
+    (display "First matrix: ")
+    (displayln secondM)
+      )
+       
+  
+    
+      (display "Row 1: ")
+      (displayln row1)
+      (display "Col 1: ")
+      (displayln col1)
+      (display "Row 2: ")
+      (displayln row2)
+      (display "Col 2: ")
+      (displayln col2)
+))
+
+
+;(call-with-input-file "matriz.txt" read_file)
 
 (define (m-mult m1 m2)
   (for/list ([r m1])
